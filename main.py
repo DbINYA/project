6c457552bf7683335485ba7f36aadb3aa9e8189f
user <hsbest14@yandex.ru>
2022-03-01 19:00:15 +0300

    Commit

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import pygame
+from flask import Flask
+from data.users import User
+from data import db_session
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "yandexlyceum_secret_key"
 
 
+def main():
+    db_session.global_init("db/blogs.sqlite")
+    db_sess = db_session.create_session()
+    print(db_sess.query(User).first().name)
+    # 7 пункт, все юзеры уже сделаны
+    # app.run()
+
+
+if __name__ == '__main__':
+    main()
